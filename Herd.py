3c10be3fd27916ca147164ecaa3d3a55006d65cc
tjdanielson <tessajdanielson@gmail.com>
2022-03-04 13:56:43 -0600

    Increased dino attack power and decreased robot weapon attack power to equal amounts

--- Herd.py
+++ Herd.py
@@ -7,9 +7,9 @@ class Herd:
     
     #creates a herd of dinosaurs
     def create_herd(self):
-        dino_one = Dinosaur('Rex the T-Rex', 30)
-        dino_two = Dinosaur('Petr the Pterodactyl', 20)
-        dino_three = Dinosaur('Vela the Velociraptor', 10)
+        dino_one = Dinosaur('Rex the T-Rex', 20)
+        dino_two = Dinosaur('Petr the Pterodactyl', 15)
+        dino_three = Dinosaur('Vela the Velociraptor', 15)
         self.dinosaurs.append(dino_one)
         self.dinosaurs.append(dino_two)
         self.dinosaurs.append(dino_three)
